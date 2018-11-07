3bd5c117c80a7eecad7cb34899d30002ab5676d7
Skybbles // L5474 <skybluehexapon@outlook.com>
2018-11-06 23:27:01 -0500

    added -f= option

--- modules/arg.js
+++ modules/arg.js
@@ -1,14 +1,19 @@
 const argCheck = require('./argcheck.js');
 
 module.exports = (input) => {
-  var argsOut = ['id', 'default'];
+  var argsOut = ['id', 'default', 'default']; 
+  /* some are subject to change; array elements represent...
+  0 - id [str] (permanent)
+  1 - output path [str] 
+  2 - format [str:[mp4|mp3|m4a|mov|avi|flv]]*/
 
   input.forEach((currentValue) => {
-    if(/-o=/.test(currentValue)) { //finna add other args when they are implemented
+    if(/-o=|-f=/.test(currentValue)) { //finna add other args when they are implemented
       var prefix = currentValue.split('=')[0];
       var value = currentValue.split('=')[1];
 
       if(prefix == '-o' && argCheck.path(value.replace(/\\/g, '/'))) argsOut[1] = value;
+      if(prefix == '-f' && argCheck.format(value)) argsOut[2] = value;
     } else argsOut[0] = currentValue;
   })
   return argsOut;
--- modules/argcheck.js
+++ modules/argcheck.js
@@ -7,3 +7,7 @@ module.exports.path = (path) => {
   if(!path.includes('/', path.length - 1)) path = `${path}/`
   if(fs.existsSync(path)) return true; else log.fatal(errs.badOutPath);
 }
+
+module.exports.format = (input) => {
+if(input.match(/mp4|mp3|m4a|mov|avi|flv/)) return true; else log.fatal(errs.badFormat);
+}
