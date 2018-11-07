89cfcd6566e313dd1ff03ff657a95b9daa27b437
Skybbles // L5474 <skybluehexapon@outlook.com>
2018-11-06 23:21:18 -0500

    :facepalm:

--- modules/log.js
+++ modules/log.js
@@ -1,3 +1,3 @@
 module.exports.err = (args) => console.log(`[ERR!] ${args}`);
 module.exports.info = (args) => console.log(`[INFO] ${args}`);
-module.exports.fatal = (args) => console.log(`[ERR!] ${args}`); process.exit();
+module.exports.fatal = (args) => console.log(`[ERR!] ${args}`); process.exit;
